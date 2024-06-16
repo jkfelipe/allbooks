dfc0c30ff41716ee336121ac793900379033bf50
Kalan Felipe Trentin <kalanfelipetrentin@gmail.com>
2024-06-15 21:27:16 -0300

    Update server.js via github

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
+  console.log("Boas-vindas a API")
   console.log("Acesse essa API em http://localhost:8000")
 })
